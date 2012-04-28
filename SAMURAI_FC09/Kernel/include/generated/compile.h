c3c95edce4fc5d9f91180a1d6d8e6ab6830ae36b
Justin D. Maggiolo <earthbound.iap@gmail.com>
2012-04-28 14:29:36 -0400

    initial fc09 source commit

---
+++ SAMURAI_FC09/Kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Thu Apr 12 10:21:35 KST 2012"
+#define LINUX_COMPILE_TIME "10:21:35"
+#define LINUX_COMPILE_BY "joohwani.kim"
+#define LINUX_COMPILE_HOST "joohwankim-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
